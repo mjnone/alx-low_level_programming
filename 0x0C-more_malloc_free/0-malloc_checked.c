70b25690d1c87b291085901177bf7264b6303e42
mjnone <moujjaneyasmine@gmail.com>
2023-08-10 12:06:11 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * Allocates memory using malloc.
- * 
- * @param b The number of bytes to allocate.
- * @return A pointer to the allocated memory.
- * If malloc fails, the function causes process termination with a status value of 98.
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
-    if (ptr == NULL)
-        exit(98);
-    return ptr;
-}
+	void *ptr;
 
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
 
