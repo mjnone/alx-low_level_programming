5c16b3088fdfee33f2c97dc3b012af553ba8e1ab
mjnone <moujjaneyasmine@gmail.com>
2023-08-11 09:32:31 -0700

    Task 4

---
+++ 0x0C-more_malloc_free/00-realloc.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_realloc - cree un nouveau block de memoire fait par malloc et free
+ * @ptr: pointeur a la memoire faite par malloc
+ * @old_size: taille de la memoire
+ * @new_size: nouvelle taille du nouveau block de memoire
+ * Return: pointeur au nouveau block de memoire
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
+}
+
