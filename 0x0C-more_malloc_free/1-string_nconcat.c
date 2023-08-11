64161be821cd119f7061f5d811ea58894f05ea42
mjnone <moujjaneyasmine@gmail.com>
2023-08-11 08:53:06 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,42 +9,38 @@
  *
  * Return: pointer to the resulting string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-    unsigned int s1_length = 0;
-    while (s1[s1_length] != '\0')
-        s1_length++;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-    unsigned int s2_length = 0;
-    while (s2[s2_length] != '\0')
-        s2_length++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-    if (n >= s2_length)
-        n = s2_length;
+	if (!s)
+		return (NULL);
 
-    char *str = malloc(s1_length + n + 1);
-    if (str == NULL)
-    {
-        return (NULL);
-    }
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-    unsigned int i, j;
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-    for (i = 0; s1[i] != '\0'; i++)
-        str[i] = s1[i];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-    for (j = 0; j < n; j++)
-    {
-        str[i] = s2[j];
-        i++;
-    }
+	s[i] = '\0';
 
-    str[i] = '\0';
-    return (str);
+	return (s);
 }
-
