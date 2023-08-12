b35d455d913ea9f00fbb20ac8e5a829f8a9b81d8
mjnone <moujjaneyasmine@gmail.com>
2023-08-12 12:21:47 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always O
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 97;
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			continue;
+			n++;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
