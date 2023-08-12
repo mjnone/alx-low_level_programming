69b1e5572c46e8267251385ece54f9bb0b0b81d7
mjnone <moujjaneyasmine@gmail.com>
2023-08-12 11:46:37 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 97;
+	m = 65;
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
+
