dd699556e9084f4f16948177b8a5fb04c1de1ff0
mjnone <moujjaneyasmine@gmail.com>
2023-08-13 07:01:38 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+        int n;
+
+        n = 122;
+        while (n >= 97)
+        {
+                putchar(n);
+                n--;
+        }
+        putchar('\n');
+        return (0);
+}
+
