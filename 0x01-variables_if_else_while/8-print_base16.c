04ad28f281ae4c9304f831d86c0171cb1504f732
mjnone <moujjaneyasmine@gmail.com>
2023-08-15 07:25:02 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,11 @@ int main(void)
 	int i;
 	int j;
 
-	for(i = 48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for(j = 97; j < 103; j++)
+	for (j = 97; j < 103; j++)
 	{
 		putchar(j);
 	}
